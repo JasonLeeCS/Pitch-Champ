01680741d16d479fcbee9c5f7b80a81d80b5f91e
airconditioner1 <allenfchen@yahoo.com>
2023-11-24 23:07:29 -0600

    Initial Commit plus mary had a little lamb song listing front front end

---
+++ app/src/main/java/com/example/pitchchamp/RecordingPlayback.java
@@ -0,0 +1,14 @@
+package com.example.pitchchamp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecordingPlayback extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recording_playback);
+    }
+}
---
+++ app/src/main/java/com/example/pitchchamp/SongLibrary.java
@@ -0,0 +1,14 @@
+package com.example.pitchchamp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SongLibrary extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_song_library);
+    }
+}
