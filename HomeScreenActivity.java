13def9ea96b831dd51906e646e64072083e65032
Jason Lee <103540272+JasonLeeCS@users.noreply.github.com>
2023-12-07 15:08:31 -0600

    Add files via upload
    Updated names  for xml and .java files and androidmanifest.xml

--- HomeScreenActivity.java
+++ HomeScreenActivity.java
@@ -1,4 +1,13 @@
 package com.cs407.pitchchmpscreensjl;
 
-public class HomeScreenActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homescreen);
+    }
 }
